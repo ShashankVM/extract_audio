25832fb1b61b1bc9934245984dab63f0cfe80d86
Shashank V M <Shashank V M>
2023-09-26 21:46:42 +0530

    added support for paralleization

--- convert_file.sh
+++ convert_file.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-for file in *$1;
-do
-newfile_name=${file// /_};
-echo $newfile_name;
-noext=$(basename ${newfile_name} .$1);
-echo $noext
-ffmpeg -i "${file}" -vn ${noext}.$2;
-done
+source_extension=$1
+destination_extension=$2
+list_of_filenames=(*$1)
+
+convert_file() {
+local filename=$1
+local source_extension=$2
+local destination_extension=$3
+newfile_name=${filename// /_};
+noext=$(basename ${newfile_name} .$source_extension);
+ffmpeg -nostats -loglevel error -i "${filename}" -vn ${noext}.$destination_extension;
+}
+
+export -f convert_file
+
+parallel convert_file ::: "${list_of_filenames[@]}" ::: $source_extension ::: $destination_extension
